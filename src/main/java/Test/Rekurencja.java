42b0a8f10a1af64b97b428c77fdcbeb0c5558b64
Robert Sadowksi <robertsadowski005@gmail.com>
2018-10-27 13:30:33 +0200

    Rename class plus adding .gitignore file.

--- src/main/java/Test/Rekurencja.java
+++ src/main/java/Test/Rekurencja.java
@@ -3,13 +3,13 @@ package Test;
 import java.util.Arrays;
 import java.util.List;
 
-public class Reckurencja {
+public class Rekurencja {
     public static void main(String[] args) {
 
-        List<Integer> chain = Arrays.asList(1,2,3,8,5,3,8);
+        List<Integer> chain = Arrays.asList(1, 2, 3, 8, 5, 3, 8);
         System.out.println(chain);
         int length = chain.size();
-        writeOnScreenBackwards(chain,length);
+        writeOnScreenBackwards(chain, length - 1);
     }
 
     private static void writeOnScreenBackwards(List<Integer> array, int length) {
@@ -17,9 +17,8 @@ public class Reckurencja {
         if (length < 0) {
             return;
         }
-        System.out.println(array.get(length));
+        System.out.print(array.get(length));
         writeOnScreenBackwards(array, length - 1);
 
-        System.out.println(length);
     }
 }
